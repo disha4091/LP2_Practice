ca91ab736e3ed556c7d96db7fc5576ae97a74c71
disha4091 <disha4091@gmail.com>
2022-05-11 12:49:38 +0530

    loop added in p9 ccl

--- CCL/p9/main.py
+++ CCL/p9/main.py
@@ -24,14 +24,15 @@ class MainPage(webapp2.RequestHandler):
         url = base_url + showname 
         data = urllib.urlopen(url).read()
         data = json.loads(data)
-        print(data)
+        #print(data)
         template_values = {
-            "name": data[0]['show']['name'] or "not available",
-            "type":data[0]['show']['type'] or "not available",
-            "language":data[0]['show']['language'] or "not available",
-            "officialSite":data[0]["show"]['officialSite'] or "not available",
-            "schedule":data[0]["show"]['schedule'] or "not available"
+            "name": [data[0]['show']['name'],data[1]['show']['name'],data[2]['show']['name']] or "not available",
+            "type":[data[0]['show']['type'],data[1]['show']['type'],data[2]['show']['type']] or "not available",
+            "language":[data[0]['show']['language'],data[1]['show']['language'],data[2]['show']['language']] or "not available",
+            "officialSite":[data[0]["show"]['officialSite'],data[1]["show"]['officialSite'],data[2]["show"]['officialSite']] or "not available",
+            "schedule":[data[0]["show"]['schedule']['time'],data[1]["show"]['schedule']['time'],data[2]["show"]['schedule']['time']] or "not available"
         }
+        print(template_values)
         path = os.path.join(os.path.dirname(__file__), 'result.html')
         self.response.out.write(template.render(path, template_values))    
 
